0d436595c5285c0dcafc33cda4815b2f541f768b
kabirussef <zezinfreestyle1@hotmail.com>
2020-10-02 21:11:03 -0300

    Print Snake

--- snake.py
+++ snake.py
@@ -1,14 +1,28 @@
 import pygame
 from pygame.locals import *
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 pygame.init()
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Snake')
 
+snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 196, 0))
+my_direction = LEFT
+
 while True:
-    
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+
+    screen.fill((0, 0, 0))
+    for pos in snake:
+        screen.blit(snake_skin, pos)
 
     pygame.display.update()
